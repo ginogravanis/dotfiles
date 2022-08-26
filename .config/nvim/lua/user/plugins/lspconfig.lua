81e72bfbcb5b36d1c9ca32a35a5f967ee73347b3
Gino Gravanis <ginogravanis@users.noreply.github.com>
2022-08-26 14:53:41 +0200

    [vim] Add LSP keymaps

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,7 +1,28 @@
-require('lspconfig').bashls.setup{}
-require('lspconfig').elmls.setup{}
-require('lspconfig').jsonls.setup{}
+local buf_option = vim.api.nvim_buf_set_option
+local buf_keymap = require('user.lib.utils').buf_keymap
+
+local on_attach = function(_, bufnr)
+   buf_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+   buf_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+   buf_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.definition()<cr>')
+   buf_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+   buf_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+   buf_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
+   buf_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+end
+
+require('lspconfig').bashls.setup{
+   on_attach = on_attach,
+}
+require('lspconfig').elmls.setup{
+   on_attach = on_attach,
+}
+require('lspconfig').jsonls.setup{
+   on_attach = on_attach,
+}
 require('lspconfig').sumneko_lua.setup{
+   on_attach = on_attach,
    settings = {
       Lua = {
          diagnostics = {
@@ -10,4 +31,6 @@ require('lspconfig').sumneko_lua.setup{
       },
    },
 }
-require('lspconfig').pylsp.setup{}
+require('lspconfig').pylsp.setup{
+   on_attach = on_attach,
+}
