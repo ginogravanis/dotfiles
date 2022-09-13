22a52cdc55f11bf1e630e23e5ed2be0cf9e60042
Gino Gravanis <ginogravanis@users.noreply.github.com>
2022-09-13 23:23:30 +0200

    [vim] Add keymaps for lsp diagnostics

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,9 +1,13 @@
+vim.keymap.set('n', '<A-j>', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+vim.keymap.set('n', '<A-k>', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
 local on_attach = function(_, bufnr)
    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { buffer = bufnr })
    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { buffer = bufnr })
    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', { buffer = bufnr })
+   vim.keymap.set('n', 'gr', ':Telescope lsp_references<cr>', { buffer = bufnr })
    vim.keymap.set('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { buffer = bufnr })
    vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = bufnr })
    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', { buffer = bufnr })
