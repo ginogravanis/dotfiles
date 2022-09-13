4c760dacf61907a586aedf2f7896afd599a8ec71
Gino Gravanis <ginogravanis@users.noreply.github.com>
2022-09-13 14:56:50 +0200

    [vim] Tweak lspconfig key bindings

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -4,8 +4,8 @@ local buf_keymap = require('user.lib.utils').buf_keymap
 local on_attach = function(_, bufnr)
    buf_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-   buf_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-   buf_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.definition()<cr>')
+   buf_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+   buf_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
    buf_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
    buf_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
    buf_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
